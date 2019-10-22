dea4ae4ed2ed013d32f1b2721526c535e6c9294f
Ajay Banstola <ajay.banstola@gmail.com>
2019-10-22 21:07:35 +0545

    updated navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,65 +1,56 @@
 import React from "react";
 
 const Navbar = () => {
-  return <div className="ui container">dsfksdfdsfsdfdsf</div>;
-  // <nav class="navbar navbar-expand-md navbar-dark bg-dark">
-  //   <div class="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
-  //     <ul class="navbar-nav mr-auto">
-  //       <li class="nav-item active">
-  //         <a class="nav-link" href="#">
-  //           Left
-  //         </a>
-  //       </li>
-  //       <li class="nav-item">
-  //         <a class="nav-link" href="//codeply.com">
-  //           Codeply
-  //         </a>
-  //       </li>
-  //       <li class="nav-item">
-  //         <a class="nav-link" href="#">
-  //           Link
-  //         </a>
-  //       </li>
-  //       <li class="nav-item">
-  //         <a class="nav-link" href="#">
-  //           Link
-  //         </a>
-  //       </li>
-  //       <li class="nav-item">
-  //         <a class="nav-link" href="#">
-  //           Link
-  //         </a>
-  //       </li>
-  //     </ul>
-  //   </div>
-  //   <div class="mx-auto order-0">
-  //     <a class="navbar-brand mx-auto" href="#">
-  //       Navbar 2
-  //     </a>
-  //     <button
-  //       class="navbar-toggler"
-  //       type="button"
-  //       data-toggle="collapse"
-  //       data-target=".dual-collapse2"
-  //     >
-  //       <span class="navbar-toggler-icon"></span>
-  //     </button>
-  //   </div>
-  //   <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
-  //     <ul class="navbar-nav ml-auto">
-  //       <li class="nav-item">
-  //         <a class="nav-link" href="#">
-  //           Right
-  //         </a>
-  //       </li>
-  //       <li class="nav-item">
-  //         <a class="nav-link" href="#">
-  //           Link
-  //         </a>
-  //       </li>
-  //     </ul>
-  //   </div>
-  // </nav>
+  return (
+    <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+      <li className="navbar-brand active">
+        <a className="navbar-brand" href="#">
+          Contact Keeper
+        </a>
+      </li>
+      <div className="navbar-collapse collapse w-50 order-1 order-md-0 dual-collapse2">
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <a className="nav-link" href="#"></a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Link
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Link
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div className="mx-auto order-0">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target=".dual-collapse2"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+      </div>
+      <div className="navbar-collapse collapse w-100 order-3 dual-collapse2">
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Right
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Link
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
