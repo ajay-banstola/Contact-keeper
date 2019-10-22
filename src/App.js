d5f4e047568ae772dfa57c823488f3c6f59fff18
Ajay Banstola <ajay.banstola@gmail.com>
2019-10-22 20:29:43 +0545

    added semantic ui

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import React from 'react';
-import './App.css';
-import Navbar from './components/Navbar';
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
 
-function App() {
-  return ( 
-    <div className = "App" >
-    <Navbar/>                 
-    <Navbar/>
+const App = () => {
+  return (
+    <div>
+      <Navbar />
     </div>
   );
-}
+};
 
 export default App;
