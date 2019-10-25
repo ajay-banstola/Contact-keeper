d849972a92012a9a91613e0b71ebca38da39423d
Ajay Banstola <ajay.banstola@gmail.com>
2019-10-25 08:05:00 +0545

    added favicon

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import Router from 'react-router-dom';
 
 const App = () => {
   return (
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,54 +2,56 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-md navbar-dark bg-dark">
-      <li className="navbar-brand active">
-        <a className="navbar-brand" href="#">
-          Contact Keeper
+    <div className="container">
+      <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+        <li className="navbar-brand active">
+          <a className="navbar-brand" href="#">
+            Contact Keeper
         </a>
-      </li>
-      <div className="navbar-collapse collapse w-50 order-1 order-md-0 dual-collapse2">
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <a className="nav-link" href="#"></a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              Link
+        </li>
+        {/* <div className="navbar-collapse collapse w-50 order-1 order-md-0 dual-collapse2">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link" href="#"></a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Link
             </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              Link
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Link
             </a>
-          </li>
-        </ul>
-      </div>
-      <div className="mx-auto order-0">
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target=".dual-collapse2"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-      </div>
-      <div className="navbar-collapse collapse w-100 order-3 dual-collapse2">
-        <ul className="navbar-nav ml-auto">
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              Right
+            </li>
+          </ul>
+        </div> */}
+        <div className="mx-auto order-0">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target=".dual-collapse2"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+        </div>
+        <div className="navbar-collapse collapse w-100 order-3 dual-collapse2">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <a className="nav-link" href="/signup">
+                Signup
             </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              Link
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Login
             </a>
-          </li>
-        </ul>
-      </div>
-    </nav>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
   );
 };
 
